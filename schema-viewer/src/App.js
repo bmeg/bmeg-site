c658c3dcf06d9f33fa6a87459343daf48a60a21b
matthewpeterkort <matthewpeterkort@gmail.com>
2025-09-01 10:09:41 -0700

    fix graph component names in schema viewer

--- schema-viewer/src/App.js
+++ schema-viewer/src/App.js
@@ -1,13 +1,12 @@
 import cytoscape from "cytoscape";
-import React, { Component } from 'react';
-import ReactJson from 'react-json-view';
+import React, { Component } from "react";
+import ReactJson from "react-json-view";
 import _ from "underscore";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-
   constructor(props) {
-    super(props)   
+    super(props);
     this.state = {
       selection: {},
       error: "",
@@ -17,239 +16,285 @@ class App extends Component {
         nodess: [],
         edges: [],
       },
-      cyElements: null
-    }
+      cyElements: null,
+    };
 
-    this.handleSelect = this.handleSelect.bind(this)
-    this.schemaQuery = this.schemaQuery.bind(this)
-    this.listGraphs = this.listGraphs.bind(this)
-    this.build = this.build.bind(this)
+    this.handleSelect = this.handleSelect.bind(this);
+    this.schemaQuery = this.schemaQuery.bind(this);
+    this.listGraphs = this.listGraphs.bind(this);
+    this.build = this.build.bind(this);
   }
 
   listGraphs() {
-    console.log("listing graphs...")
-    fetch( "/v1/graph", {
+    console.log("listing graphs...");
+    fetch("/v1/graph", {
       method: "GET",
-    }).then(function(response) {
-      if (!response.ok) {
-        var err = "ERROR: GET " + response.url + " " + response.status + " " + 
-            response.statusText
-        console.log(err)
-        this.setState({error: err})
-      }
-      return response.json()
-    }.bind(this)).then(function(json) {
-      var graphs = json['graphs'].filter(function(g) {
-					return g.endsWith("__schema__")
-			}).map(x => x.replace("__schema__", ""))
-      console.log("found graphs:", graphs)
-      if (graphs.includes(this.state.graph)) {
-        this.setState({graphs: graphs})
-      } else {
-        console.log("default graph", this.state.graph, "not found")
-        this.setState({graphs: graphs, graph: ""})
-      }
-    }.bind(this)).catch(err => {
-      console.log("ERROR:", err)
-      err = "ERROR: No graphs found"
-      console.log(err)
-      this.setState({error: err})
     })
+      .then(
+        function (response) {
+          if (!response.ok) {
+            var err =
+              "ERROR: GET " +
+              response.url +
+              " " +
+              response.status +
+              " " +
+              response.statusText;
+            console.log(err);
+            this.setState({ error: err });
+          }
+          return response.json();
+        }.bind(this),
+      )
+      .then(
+        function (json) {
+          var graphs = json["graphs"]
+            .filter(function (g) {
+              return g.endsWith("__schema__");
+            })
+            .map((x) => x.replace("__schema__", ""));
+          console.log("found graphs:", graphs);
+          if (graphs.includes(this.state.graph)) {
+            this.setState({ graphs: graphs });
+          } else {
+            console.log("default graph", this.state.graph, "not found");
+            this.setState({ graphs: graphs, graph: "" });
+          }
+        }.bind(this),
+      )
+      .catch((err) => {
+        console.log("ERROR:", err);
+        err = "ERROR: No graphs found";
+        console.log(err);
+        this.setState({ error: err });
+      });
   }
 
   schemaQuery(graph) {
-    console.log("Getting the schema for graph: ", graph)
-    fetch( "/v1/graph/" + graph + "/schema", {
+    console.log("Getting the schema for graph: ", graph);
+    fetch("/v1/graph/" + graph + "/schema", {
       method: "GET",
-    }).then(function(response) {
-      if (!response.ok) {
-        var err = "ERROR: GET " + response.url + " " + response.status + " " + 
-            response.statusText
-        console.log(err)
-        this.setState({error: err})
-      }
-      return response.json()
-    }.bind(this)).then(function(json) {
-      var edges = json["edges"].map(function(x){
-        return {
-          "data": {
-            "id": x["gid"], 
-            "label": x["label"], 
-            "source": x["from"], 
-            "target": x["to"]
-          }, 
-          "classes": "autorotate"
-        }
-      })
-      var nodes = json["vertices"].map(function(x){
-        return {"data": {"id": x["gid"]}}
-      })
-      this.setState({elements: {"nodes": nodes, "edges": edges}, schema: json})
-    }.bind(this)).catch(err => {
-      console.log("ERROR:", err)
-      err = "ERROR: Failed to load the schema"
-      console.log(err)
-      this.setState({error: err})
     })
-    console.log("Loaded the schema for graph: ", graph)
+      .then(
+        function (response) {
+          if (!response.ok) {
+            var err =
+              "ERROR: GET " +
+              response.url +
+              " " +
+              response.status +
+              " " +
+              response.statusText;
+            console.log(err);
+            this.setState({ error: err });
+          }
+          return response.json();
+        }.bind(this),
+      )
+      .then(
+        function (json) {
+          var edges = json["edges"].map(function (x) {
+            return {
+              data: {
+                id: x["id"],
+                label: x["label"],
+                source: x["from"],
+                target: x["to"],
+              },
+              classes: "autorotate",
+            };
+          });
+          var nodes = json["vertices"].map(function (x) {
+            return { data: { id: x["id"] } };
+          });
+          this.setState({
+            elements: { nodes: nodes, edges: edges },
+            schema: json,
+          });
+        }.bind(this),
+      )
+      .catch((err) => {
+        console.log("ERROR:", err);
+        err = "ERROR: Failed to load the schema";
+        console.log(err);
+        this.setState({ error: err });
+      });
+    console.log("Loaded the schema for graph: ", graph);
   }
 
   // handle graph selections
   handleSelect(event) {
-    console.log("selected graph:", event.target.value)
-    this.setState({graph: event.target.value, error: "", selection: {}})
-    this.schemaQuery(event.target.value)
+    console.log("selected graph:", event.target.value);
+    this.setState({ graph: event.target.value, error: "", selection: {} });
+    this.schemaQuery(event.target.value);
   }
 
   componentDidMount() {
-    this.listGraphs()
+    this.listGraphs();
     if (!_.isEqual(this.state.graph, "")) {
-      this.schemaQuery(this.state.graph)
+      this.schemaQuery(this.state.graph);
     }
-  	this.build()
+    this.build();
   }
 
   componentDidUpdate() {
-  	this.build()
+    this.build();
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return true
+    return true;
   }
 
   build() {
     if (_.isEqual(this.state.graph, "")) {
-      return
+      return;
     }
     if (_.isEqual(this.state.elements, this.state.cyElements)) {
-      return
+      return;
     }
 
-    console.log("Cytoscape.js is rendering the graph...")
+    console.log("Cytoscape.js is rendering the graph...");
 
-    var cy = cytoscape(
-      {
-        container: document.getElementById("cy"),
+    var cy = cytoscape({
+      container: document.getElementById("cy"),
 
-        boxSelectionEnabled: false,
-        autounselectify: false,
+      boxSelectionEnabled: false,
+      autounselectify: false,
 
-        minZoom: 0.1,
-        maxZoom: 10,
+      minZoom: 0.1,
+      maxZoom: 10,
 
-        elements: this.state.elements,
+      elements: this.state.elements,
 
-        style: cytoscape.stylesheet()
-          .selector("node")
-          .css({
-            "height": 80,
-            "width": 80,
-            "background-fit": "cover",
-            'background-color': "#bcbcbc",
-            "border-color": "#bcbcbc",
-            "font-size": "14px",
-            "border-width": 3,
-            "border-opacity": 1,
-            "text-valign": "center",
-            "label": "data(id)"
-          })
-          .selector("node:selected")
-          .css({
-            'background-color': "#4286f4",
-            "border-color": "#4286f4",
-          })
-          .selector("edge")
-          .css({
-            "width": 6,
-            "target-arrow-shape": "triangle",
-            "line-color": "#ffaaaa",
-            "target-arrow-color": "#ffaaaa",
-            "curve-style": "bezier",
-            "label": "data(label)"
-          })
-          .selector("edge:selected")
-          .css({
-            "line-color": "#4286f4",
-            "target-arrow-color": "#4286f4",
-          })
-          .selector(".autorotate")
-          .css({
-            "edge-text-rotation": "autorotate"
-          }),
+      style: cytoscape
+        .stylesheet()
+        .selector("node")
+        .css({
+          height: 80,
+          width: 80,
+          "background-fit": "cover",
+          "background-color": "#bcbcbc",
+          "border-color": "#bcbcbc",
+          "font-size": "14px",
+          "border-width": 3,
+          "border-opacity": 1,
+          "text-valign": "center",
+          label: "data(id)",
+        })
+        .selector("node:selected")
+        .css({
+          "background-color": "#4286f4",
+          "border-color": "#4286f4",
+        })
+        .selector("edge")
+        .css({
+          width: 6,
+          "target-arrow-shape": "triangle",
+          "line-color": "#ffaaaa",
+          "target-arrow-color": "#ffaaaa",
+          "curve-style": "bezier",
+          label: "data(label)",
+        })
+        .selector("edge:selected")
+        .css({
+          "line-color": "#4286f4",
+          "target-arrow-color": "#4286f4",
+        })
+        .selector(".autorotate")
+        .css({
+          "edge-text-rotation": "autorotate",
+        }),
 
-        layout: {
-          name: "cose"
-        }
-      }
-    )
+      layout: {
+        name: "cose",
+      },
+    });
 
-    cy.on('tap', 'edge', event => {
-      var targetEdge = event.target.data().id
-      var data = {}
+    cy.on("tap", "edge", (event) => {
+      var targetEdge = event.target.data().id;
+      var data = {};
       for (var i = 0; i < this.state.schema.edges.length; i++) {
-        if (this.state.schema.edges[i].gid === targetEdge) {
-          data = this.state.schema.edges[i]
+        if (this.state.schema.edges[i].id === targetEdge) {
+          data = this.state.schema.edges[i];
         }
       }
-      this.setState({ selection: data })
-    })
-    cy.on('tap', 'node', event => {
-      var targetVertex = event.target.data().id
-      var data = {}
+      this.setState({ selection: data });
+    });
+    cy.on("tap", "node", (event) => {
+      var targetVertex = event.target.data().id;
+      var data = {};
       for (var i = 0; i < this.state.schema.vertices.length; i++) {
-        if (this.state.schema.vertices[i].gid === targetVertex) {
-          data = this.state.schema.vertices[i]
+        if (this.state.schema.vertices[i].id === targetVertex) {
+          data = this.state.schema.vertices[i];
           // add some custom logic
           if (_.isEqual(data.label, "GeneExpression")) {
-            data.data.values = {"ENSG00000004059": "STRING",
-                                "...": "STRING"}
+            data.data.values = { ENSG00000004059: "STRING", "...": "STRING" };
           } else if (_.isEqual(data.label, "TranscriptExpression")) {
-            data.data.values = {"ENST00000000233": "STRING",
-                                "...": "STRING"}
+            data.data.values = { ENST00000000233: "STRING", "...": "STRING" };
           } else if (_.isEqual(data.label, "CopyNumberAlteration")) {
-            data.data.values = {"ENSG00000004059": "STRING",
-                                "...": "STRING"}
+            data.data.values = { ENSG00000004059: "STRING", "...": "STRING" };
           }
         }
       }
-      this.setState({ selection: data })
-    })
-    this.cy = cy
-    this.setState({ cyElements: this.state.elements })
+      this.setState({ selection: data });
+    });
+    this.cy = cy;
+    this.setState({ cyElements: this.state.elements });
   }
 
   render() {
-    let selectStyle = {width: "15%", height: "2em", fontSize: "1.25em", 
-                       margin: "10px auto", display: "block"}
-    let optionItems = this.state.graphs.map(
-      (graph) => <option key={graph}>{graph}</option>
-    )
+    let selectStyle = {
+      width: "15%",
+      height: "2em",
+      fontSize: "1.25em",
+      margin: "10px auto",
+      display: "block",
+    };
+    let optionItems = this.state.graphs.map((graph) => (
+      <option key={graph}>{graph}</option>
+    ));
     let cyStyle = {
       height: this.props.dataset.height,
       width: this.props.dataset.width,
       margin: "5px auto",
       borderStyle: "solid",
       borderColor: "#D3D3D3",
-      borderWidth: "thin"
-    }
+      borderWidth: "thin",
+    };
     return (
       <div>
         <div id="selectGraph">
-          <select style={selectStyle} value={this.state.graph} onChange={this.handleSelect}>
-            <option value="" disabled>Select Graph</option>
+          <select
+            style={selectStyle}
+            value={this.state.graph}
+            onChange={this.handleSelect}
+          >
+            <option value="" disabled>
+              Select Graph
+            </option>
             {optionItems}
           </select>
         </div>
         <div id="errorMessage">
-          <h4 style={{color: "red", textAlign: "center"}}>{this.state.error}</h4>
+          <h4 style={{ color: "red", textAlign: "center" }}>
+            {this.state.error}
+          </h4>
         </div>
         <div style={cyStyle} id="cy"></div>
-        <div style={{width: this.props.dataset.width, margin: "5px auto"}} id="reactJson">
-          <ReactJson src={this.state.selection} name={false}  enableClipboard={false} displayDataTypes={false}/>
+        <div
+          style={{ width: this.props.dataset.width, margin: "5px auto" }}
+          id="reactJson"
+        >
+          <ReactJson
+            src={this.state.selection}
+            name={false}
+            enableClipboard={false}
+            displayDataTypes={false}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
